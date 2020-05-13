e23b0644748599844e5bf338f57e19b4237ff8d2
unknown <prakashbahadursingh2@gmail.com>
2020-05-13 13:11:29 -0700

    required dependencies are installed and create store and provider libraries are imported into the index.js file and reducer file called carSaleReducer.js created in which initialState and empty reduce function is created whic are exported into redux.js file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//below new
+import {createStore} from 'redux';
+import {carSaleReducer} from './reducers/carSaleReducer';
+import { Provider } from 'react-redux';
+// above new
+
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//below new
+const store = createStore(carSaleReducer);
+console.log(store.getState(), 'What we getting here???????!!!!!!')
+// above new
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render (<Provider store = {store}> <App /> </Provider>, rootElement);
