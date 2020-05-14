2bd44e0612b0a90b1531e3ea51aed0b489c90b44
unknown <prakashbahadursingh2@gmail.com>
2020-05-13 17:55:08 -0700

    store created and app component is wrapped with in <provider> to provide data to the app from the store, higher order component is imported into the app componnet, then connect function created which call mapStateToProps and pass in the entire redux store as an argument(connect function connect function connect app component to the redux store then using mapStateToProps function map the state to props by calling twice first call map state to props and returns an object and second call is passing inthe component that we want to connect to the redux store.).

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { connect } from 'react-redux';//it is a Higher Order Component (similar to custom hooks)
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+
+
+
+
+const App = state  => {
+  // const state = {
+  //   additionalPrice: 0,
+  //   car: {
+  //     price: 26395,
+  //     name: '2019 Ford Mustang',
+  //     image:
+  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+  //     features: []
+  //   },
+  //   additionalFeatures: [
+  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 2, name: 'Racing detail package', price: 1500 },
+  //     { id: 3, name: 'Premium sound system', price: 500 },
+  //     { id: 4, name: 'Rear spoiler', price: 250 }
+  //   ]
+  // }; these moved to reducer file.
 
   return (
     <div className="boxes">
@@ -37,4 +42,23 @@ const App = () => {
   );
 };
 
-export default App;
+//bSTP returns an object - each property on that object is added to the props of the connected component
+//the connect function will call mSTP  and pass in the entire redux store as an argument!
+const mapStateToProps = state=> {
+  console.log({state},' What We Have Now?????!!!!!')
+  //so the state has props of additionalFeatures, additionalPrice, car(features: [image, name, price])
+  return{
+    itsTooHard: 100,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice
+
+  }
+}
+
+export default connect(mapStateToProps, {}) (App);
+//////....[{"second", need to connect App component ti the redux store then using mapStateToProps function to ...map the state(from the redux store) to props (of the connected component) }]....////// follow below---->
+
+//here connect is called twice
+//1st call is passing in the mSTP(Map State To Props) and an object
+// 2nd call is passing in the component we want to connect to the Redux store.
